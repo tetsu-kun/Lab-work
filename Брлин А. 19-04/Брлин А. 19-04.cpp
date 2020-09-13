4c2f95404f861ad28d3085bc845d64f0aee040ea
tetsu-kun <70638661+tetsu-kun@users.noreply.github.com>
2020-09-13 17:11:47 +0300

    Add files via upload
    Лабораторная работа 0

---
+++ Брлин А. 19-04/Брлин А. 19-04.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello World!\n";
+
+   setlocale(LC_ALL, "Rus");
+
+   int peremen;
+
+   cout << "Введите число:";
+   cin >> peremen;
+
+   cout << "Ваше число = " << peremen;
+}
+
+
+
